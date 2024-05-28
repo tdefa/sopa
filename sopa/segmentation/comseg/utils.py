ec6f052fd7854815f2102b220313dc59657002ff
tdefa <thomas.defard@minesparistech.fr>
2024-05-28 21:55:37 +0200

    type

--- sopa/segmentation/comseg/utils.py
+++ sopa/segmentation/comseg/utils.py
@@ -2,11 +2,27 @@
 from spatialdata.models import PointsModel
 import pandas as pd
 from sopa._sdata import to_intrinsic
+from spatialdata import SpatialData
+
+gene_column: str | None = None,
+area_ratio_threshold: float = 0.25,
 
 
-def add_centroids_to_sdata(sdata,
-                           points_key="transcripts",
-                           shapes_key='cellpose_boundaries', z_constant=None):
+def add_centroids_to_sdata(sdata: SpatialData,
+                           points_key: str = "transcripts",
+                           shapes_key: str = 'cellpose_boundaries',
+                           z_constant: float | None = None) -> SpatialData:
+    """
+    Function to add centroids to the sdata
+    Args:
+        sdata:
+        points_key: name of the RNA coodirdiante in sdata
+        shapes_key: name of the cellpose boundaries in sdata
+        z_constant: this value corresponds to the z value of the cell centroid.
+
+    Returns: sdata with the centroids
+
+    """
     centroid = sdata[shapes_key].geometry.centroid
     x_centroid = list(centroid.geometry.x)
     y_centroid = list(centroid.geometry.y)
